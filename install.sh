c7199fd729f58541388273b01f86fabf6cb52d1b
Trevor Rothaus <trevorrothaus@Trevor-R-2.0>
2016-03-22 14:17:08 -0700

    added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# http://brew.sh/
+echo "==================="
+echo "INSTALLING HOMEBREW"
+echo "==================="
+echo ""
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
