9d8ea2422ba9aa225f205912311ab515139028e6
Trevor Rothaus <trevorrothaus@Trevor-R-2.0>
2016-03-22 14:41:13 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -7,3 +7,29 @@ echo "==================="
 echo ""
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo ""
+echo "==================="
+echo "INSTALLING CHEFDK"
+echo "==================="
+echo ""
+brew install caskroom/cask/brew-cask
+brew cask install chefdk
+eval "$(chef shell-init bash)"
+
+
+echo ""
+echo "==================="
+echo "INSTALLING VIRTUALBOX"
+echo "==================="
+echo ""
+brew install caskroom/cask/brew-cask
+brew cask install virtualbox
+
+echo ""
+echo "==================="
+echo "INSTALLING VAGRANT"
+echo "==================="
+echo ""
+brew install caskroom/cask/brew-cask
+brew cask install vagrant
+brew tap homebrew/completions && brew install vagrant-completion # Optional
